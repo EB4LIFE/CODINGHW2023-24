7d4e47f29d3a6de965ba525db7ae3d278fbb0768
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-01-15 07:13:19 -0500

    Update HWSORTINGHATEB.cpp

--- HWSORTINGHATEB.cpp
+++ HWSORTINGHATEB.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+#include <ctime>
+using namespace std;
+
+/*This program will produce 10 random numbers and then determine if they are in a sorted list or not*/
+
+int main()  {
+  
+   int n;
+   int n2;
+	 bool  isSorted = true;
+	
+	srand(unsigned int(time));	
+	n = rand() % 1000 + 1; 
+	cout << n << " "; 
+
+	for (int i = 0; i < 9; i++) { 
+		n2 = rand() % 1000 + 1; 
+		cout << n2 << " ";
+		if (n2 < n) { 
+			isSorted = false;
+			break;
+		}
+		n = n2; 
+	}
+	if (isSorted == true) {  
+		cout << endl << "sorted list";
+	}
+	else {
+		cout << endl << "not sorted list";
+	}
+	return 0;
+}
+
