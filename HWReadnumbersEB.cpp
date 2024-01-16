342bf3852aaac284ae69289949c3f3695963ca12
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-01-16 09:01:48 +0200

    Update HWReadnumbersEB.cpp

--- HWReadnumbersEB.cpp
+++ HWReadnumbersEB.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+/* This program will take 2 numbers and read them in a list of numbers until their
+sum > the first number or the number of numbers read =
+to the second number, and then prints out the final sum - Eitan brown
+*/
+
+int main() {
+
+	int n1;
+	int n2;
+	
+
+	cout << "enter 2 positive numbers:" << endl;
+	cin >> n1;
+	while (n1 <= 0) {
+		cout << "ERROR" << endl;
+		cin >> n1;
+	}
+	cin >> n2;
+	while (n2 <= 0) {
+		cout << "ERROR" << endl;
+		cin >> n2;
+	}
+}
