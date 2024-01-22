60cda59520c6b6e901bfd8978466d1b8f562df79
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-01-22 09:39:50 +0200

    Update HW5.1GEOBLEHEB.cpp

--- HW5.1GEOBLEHEB.cpp
+++ HW5.1GEOBLEHEB.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
 
+//Function that if called will give the area of rectangle with specified values
 int Area(int rl, int rw) {
       int Rectarea;
       Rectarea = rl * rw;
       return Rectarea;
 }
-
+//Function that if called will give the area of circle with specified values
 double Area(int rad) {
     const double PI = 3.14159;
     double Cirarea;
