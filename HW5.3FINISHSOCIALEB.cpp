9064bdc9921367847839c3e9395ebc1627924ea9
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-01-23 12:44:32 +0200

    Update HW5.3FINISHSOCIALEB.cpp

--- HW5.3FINISHSOCIALEB.cpp
+++ HW5.3FINISHSOCIALEB.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+//Function that is passed a number and returns the sum of the digits
+
+//Function that is passed an id number and returns its correct check-sum.
+
+int main () {
+  
+    int ID;
+    cout << "enter your ID" << endl;
+    cin >> ID;
+  
+  return 0;
+}
