729111255977a3bc5330898eb6db2457b7577a5b
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-01-23 16:10:00 +0200

    Update HW5.4PRIMEEB.cpp

--- HW5.4PRIMEEB.cpp
+++ HW5.4PRIMEEB.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
+//Checks to see if number is prime
 bool isPrime(int pnum) {
-    int pnum;
     bool isprime;
+    for(int i = 2; i < pnum; i++) {
+        if (num1 % i == 0) { 
+		isprime = false; 
+		break;
+		}
+		else {
+		isprime = true;
+        }
+    }
+     return isprime;
+}
+//takes a positive whole number and returns the smallest prime number that is greater than or equal to the number passed.
+int findPrime(int num) {
+    int smallp;
+	bool isprime;
+	do { 
+		isprime = isPrime(num);
+		if (isprime == true) {
+			smallp = num; 
+		}
+		else { 
+			prime = false;
+			++num; 
+		}
+	} while (isprime == false); 
     
+	return smallp; 
 }
-
-int foundPrime(int num) {
-}
-
+//USER INPUT
 int main () {
-
-int usernum
+int tablesize;
+srand( (unsigned)time(NULL) ); 
+int usernum;
 cout << "Enter number of values:"
 cin >> usernum;
-while(usernum <=0) {
-cout << "ERROR" << endl;
-cin >> usernum;
+//If the number is negative, the program randomly chooses a number from 10-100
+if(usernum <0) {
+usernum = (rand() % 91 + 10);
 }
-//Applying functions from above to get wanted result
-cout << "Table size: " << _ << endl;
+tablesize = findPrime(usernum); 
+cout << "Table size: " << tablesize << endl;
 }
