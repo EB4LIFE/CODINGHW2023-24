ec5a51842c5ed0d0cc58c25cc13cd0a5bd96e704
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-01-31 16:39:42 +0200

    Update HW6.5INARRAYEB.cpp

--- HW6.5INARRAYEB.cpp
+++ HW6.5INARRAYEB.cpp
@@ -1,83 +1,97 @@
 #include<iostream>
 using namespace std;
 
-void Fillarray(int arr[], const int size);
-bool checkDif(int e1, int arr2[], int size);
-void FillDif(int arr1[], int arr2[], int arrDif[], int size);
-void PrintDif(int arrDif[], int size);
-
+void FillArray(int arr[], const int size);
+bool CheckDifference(int element1, int arr2[], int size);
+void FillDifference(int arr1[], int arr2[], int arrDiff[], int size);
+void PrintDifference(int arrDiff[], int size);
 
 int main() {
-	int set1[6];
-	int set2[6];
-	int difference[6] = { 0 };
-	cout << "enter first 6 numbers:" << endl;
-	Fillarray(set1, 6);
-	cout << "enter next 6 numbers:" << endl;
-	Fillarray(set2, 6);
-	FillDif(set1, set2, difference, 6);
-	cout << "set difference is:" << endl;
-	PrintDif(difference, 6);
-	return 0; 
+    int set1[6];
+    int set2[6]; 
+    int difference[6] = { 0 };
+
+    cout << "Enter first 6 numbers:" << endl;
+    FillArray(set1, 6);
+
+    cout << "Enter next 6 numbers:" << endl;
+    FillArray(set2, 6);
+
+    FillDifference(set1, set2, difference, 6);
+
+    cout << "Set difference is:" << endl;
+    PrintDifference(difference, 6);
+
+    return 0;
 }
 
-void Fillarray(int arr[], const int size) {
-	int userinput;
-	for (int i = 1; i < size; i++) {
-		cin >> userinput;
-		if (userinput <= 0) {
-			break;
-		}
-		else {
-			arr[i] = userinput;
-		}
-	}
-}
-bool checkDif(int e1, int arr2[], int size) {
-	//We need to see if there specific elements in the set {THIS function will be a buldingblock in the function below
-	bool jamesbonded = false; //if they are in the set or not
-	for (int i = 0; i < size; i++) {
-		if (e1 == arr2[i]) {
-			jamesbonded = true; \
-				break;
-		}
-	}
-	return jamesbonded;
-}
-void FillDif(int arr1[], int arr2[], int arrDif[], int size) {
-	//comparing elemenets of Array 1 to Array 2
-	//result will leave over any remaining elements from Array 1 into the array Difference
-	bool Jbond;
-	for (int i = 0; i < size; i++) {
-		Jbond = checkDif(arr1[i], arr2, size);
+void FillArray(int arr[], const int size) {
+    bool Error = false;
+    int input;
 
-		if (Jbond == true) {
-			arrDif[i] = 0;
-		}
-		else {
-			arrDif[i] = arr1[i];
-		}
-	}
+    do {
+        for (int i = 0; i < size; i++) {
+            cin >> input;
+            if (input > 0) {
+                arr[i] = input;
+            }
+            else {
+                Error = true;
+                cout << "ERROR" << endl;
+                break;
+            }
+        }
+    } while (Error == true);
 }
-void PrintDif(int arrDif[], int size) {
-	bool empty = true; 
-	for (int i = 0; i < size; i++) {
-		if (arrDif[i] == 0) {
-			empty = true;
-		}
-		else {
-			empty = false;
-			break;
-		}
-	}
-	if (empty==true) {
-		cout << "empty" << endl;
-	}
-	else {
-		for (int j = 0; j < size; j++) {
-			if (arrDif[j] != 0) {
-				cout << arrDif[j] << " ";
-			}
-		}
-	}
+
+bool CheckDifference(int element1, int arr2[], int size) {
+    bool inSet = false;
+
+    for (int i = 0; i < size; i++) {
+        if (element1 == arr2[i]) {
+            inSet = true;
+            break;
+        }
+    }
+    return inSet;
+}
+
+void FillDifference(int arr1[], int arr2[], int arrDiff[], int size) {
+    bool inSet2;
+
+    for (int i = 0; i < size; i++) {
+        inSet2 = CheckDifference(arr1[i], arr2, size);
+
+        if (inSet2 == true) {
+            arrDiff[i] = 0;
+        }
+        else {
+            arrDiff[i] = arr1[i];
+        }
+    }
+}
+
+void PrintDifference(int arrDiff[], int size) {
+    bool EMPTY = true;
+
+    for (int i = 0; i < size; i++) {
+        if (arrDiff[i] == 0) {
+            EMPTY = true;
+        }
+        else {
+            EMPTY = false;
+            break;
+        }
+    }
+    if (EMPTY == true) {
+        cout << "empty" << endl;
+    }
+    else {
+        for (int j = 0; j < size; j++) {
+            if (arrDiff[j] != 0) {
+                cout << arrDiff[j] << " ";
+            }
+        }
+        cout << endl;
+    }
 }
