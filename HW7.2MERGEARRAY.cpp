44e3beb23586805d64085072aa2a98cbcc5cd8cb
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-02-05 21:52:14 +0200

    Update HW7.2MERGEARRAY.cpp

--- HW7.2MERGEARRAY.cpp
+++ HW7.2MERGEARRAY.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool Sorted(int arrsort[], const int size);
 void fillArray(int arr[], const int size);
 void printArray(int arrp[], int size);
-void mergedarray(int arr1[], int arr2[], int arr3[], int mer[], int size1, int merge);
+void mergeArrays(int arr1[], int arr2[], int arr3[], int arrMerge[], int size1, int sizeMerge);
 
 
 int main() {
@@ -13,14 +13,14 @@ int main() {
 	int B[10] = { 0 };
 	int C[10] = { 0 };
     int M[30] = { 0 };
-	cout << "enter values for the first vector:" << endl;
+	cout << "Enter values for the first vector:" << endl;
 	fillArray(A, 10);
-	cout << "enter values for the second vector:" << endl;
+	cout << "Enter values for the second vector:" << endl;
 	fillArray(B, 10);
-	cout << "enter values for the third vector:" << endl;
+	cout << "Enter values for the third vector:" << endl;
 	fillArray(C, 10);
-	cout << "merged vector is:" << endl;
-    mergedarray(A, B, C, M, 10, 30);
+	cout << "Merged vector is:" << endl;
+    mergeArrays(A, B, C, M, 10, 30);
     printArray(M, 30);
 }
 
@@ -28,7 +28,10 @@ int main() {
 //stepping stone function - sort in descending order
 bool Sorted(int arrsort[], const int size) {
 	for (int i = 0; i < size - 1; i++) {
-		if (arrsort[i] < arrsort[i + 1]) {
+	    if(arrsort[i+1]==0) {
+	        return true;
+	    }
+		if (arrsort[i] <= arrsort[i + 1]) {
 			return false;
 		}
 	}
@@ -61,83 +64,85 @@ void fillArray(int arr[], const int size) {
 			continue;
 		}
 		sorted = Sorted(arr, 10);
-		if (sorted == false || positive == false) {
+		if (sorted == false) {
 			cout << "ERROR" << endl;
 		}
 	} while (sorted == false || positive == false);
 
 }
-void mergedarray(int arr1[], int arr2[], int arr3[], int mer[], int size1, int merge) {
-        int i = 0, j = 0, k = 0, l = 0;
-        while (i < size1 && j < size1 && k < merge) {
-            if (arr1[i] >= arr2[j] && arr1[i] >= arr3[k]) {
-                mer[l] = arr1[i++];
-            }
-            else if (arr2[j] >= arr1[i] && arr2[j] >= arr3[k]) {
-                mer[l] = arr2[j++];
-            }
-            else if (arr3[k] >= arr1[i] && arr3[k] >= arr2[j]) {
-                mer[l] = arr3[k++];
-            }
-            else {
-                break;
-            }
-            l++;
-        }
+void mergeArrays(int arr1[], int arr2[], int arr3[], int arrMerge[], int size1, int sizeMerge){
+        int i = 0, j = 0, k = 0, x = 0;
 
-        if (i < size1 && j < size1) {
-            while (i < size1 && j < size1) {
-                if (arr1[i] > arr2[j]) {
-                    mer[l] = arr1[i++];
-                }
-                else {
-                    mer[l] = arr2[j++];
-                }
-                l++;
-            }
-            while (i < size1) {
-                mer[l++] = arr1[i++];
-            }
-            while (j < size1) {
-                mer[l++] = arr2[j++];
-            }
-        }
+while (i < 10 && j < 10 && k < 10) {
+    if (arr1[i] >= arr2[j] && arr1[i] >= arr3[k]) {
+        arrMerge[x] = arr1[i++];
+    }
+    else if (arr2[j] >= arr1[i] && arr2[j] >= arr3[k]) {
+        arrMerge[x] = arr2[j++];
+    }
+    else if (arr3[k] >= arr1[i] && arr3[k] >= arr2[j]) {
+        arrMerge[x] = arr3[k++];
+    }
+    else {
+        break;
+    }
+    x++;
+}
 
-        if (j < size1 && k < merge) {
-            while (j < size1 && k < merge) {
-                if (arr2[j] > arr3[k]) {
-                    mer[l] = arr2[j++];
-                }
-                else {
-                    mer[l] = arr3[k++];
-                }
-                l++;
-            }
-            while (j < size1) {
-                mer[l++] = arr2[j++];
-            }
-            while (k < merge) {
-                mer[l++] = arr3[k++];
-            }
+if (i < 10 && j < 10) {
+    while (i < 10 && j < 10) {
+        if (arr1[i] > arr2[j]) {
+            arrMerge[x] = arr1[i++];
         }
+        else {
+            arrMerge[x] = arr2[j++];
+        }
+        x++;
+    }
+    while (i < 10) {
+        arrMerge[x++] = arr1[i++];
+    }
+    while (j < 10) {
+        arrMerge[x++] = arr2[j++];
+    }
+}
 
-        if (i < size1 && k < merge) {
-            while (i < size1 && k < merge) {
-                if (arr1[i] > arr3[k]) {
-                    mer[l] = arr1[i++];
-                }
-                else {
-                    mer[l] = arr3[k++];
-                }
-                l++;
-            }
-            while (i < size1) {
-                mer[l++] = arr1[i++];
-            }
-            while (k < merge) {
-                mer[l++] = arr3[k++];
-            }
+if (j < 10 && k < 10) {
+    while (j < 10 && k < 10) {
+        if (arr2[j] > arr3[k]) {
+            arrMerge[x] = arr2[j++];
         }
+        else {
+            arrMerge[x] = arr3[k++];
+        }
+        x++;
+    }
+    while (j < 10) {
+        arrMerge[x++] = arr2[j++];
+    }
+    while (k < 10) {
+        arrMerge[x++] = arr3[k++];
+    }
+}
+
+if (i < 10 && k < 10) {
+    while (i < 10 && k < 10) {
+        if (arr1[i] > arr3[k]) {
+            arrMerge[x] = arr1[i++];
+        }
+        else {
+            arrMerge[x] = arr3[k++];
+        }
+        x++;
+    }
+    while (i < 10) {
+        arrMerge[x++] = arr1[i++];
+    }
+    while (k < 10) {
+        arrMerge[x++] = arr3[k++];
+    }
+}
+
     }
 void printArray(int arrp[], int size) {
 	for (int i = 0; i < size; i++) {
@@ -146,4 +151,3 @@ void printArray(int arrp[], int size) {
 		}
 	}
 }
-
